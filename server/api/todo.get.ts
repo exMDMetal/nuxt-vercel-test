f3ef08235384698cb4810a9d57ac26201a404a7c
exMDM <exMDMetal@gmail.com>
2023-08-30 11:44:26 +0300

    updates

--- server/api/todo.get.ts
+++ server/api/todo.get.ts
@@ -1,7 +1,5 @@
 import type { Todo } from '~/types/todo'
 
 export default defineEventHandler(async (event) => {
-    const { getItem } = useStorage('default')
-
-    return await getItem<Todo[]>('todos') || []
+    return await useStorage('default').getItem<Todo[]>('todos') || []
 })
